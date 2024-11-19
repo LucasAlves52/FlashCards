3d386da9afdbdb70ddd57e3dfe04f1f3d3d26554
Lucas Alves52 <00001123844732sp@al.educacao.sp.gov.br>
2024-11-19 07:37:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,76 @@
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais grande do nosso sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Arte/História da Arte',
+    'Quem pintou a famosa obra "A Noite Estrelada"?',
+    'Vincent van Gogh'
+)
+
+criaCartao(
+    'Química',
+    ' Qual é o elemento químico mais leve?',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro "O Senhor dos Anéis"?',
+    'J.R.R. Tolkien'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Canberra'
+)
+
+criaCartao(
+    'História/Exploração Espacial',
+    'Quem foi o primeiro homem a caminhar na Lua?',
+    'Armstrong'
+)
+
+criaCartao(
+    'Geografia',
+    ' Qual é o rio mais longo do mundo?',
+    'Rio Nilo'
+)
+
+criaCartao(
+    'Física/Teoria da Relatividade',
+    'Quem foi o criador da teoria da relatividade?',
+    'Albert Einstein'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Você Sabia?',
+    'O cérebro humano pesa cerca de 1,4 kg.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Você Sabia?',
+    'A velocidade do som é de 343 m/s.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Você Sabia?',
+    'O primeiro computador foi inventado em 1822 por Charles Babbage.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Você Sabia?',
+    'O maior animal da Terra é a baleia-azul.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Você Sabia?',
+    '- A linguagem mais falada no mundo é o mandarim.'
+)
